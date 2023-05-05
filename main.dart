78fafcb80e7c30545daee7677079d23982e7f058
Ahmad77Ali <101810020+Ahmad77Ali@users.noreply.github.com>
2023-05-05 20:59:49 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,27 @@
+import './screens/sign_screen.dart';
+import './screens/app_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App',
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (ctx, userSnapshot) {
+          if (userSnapshot.hasData) {
+            return AppScreen();
+          }
+          return SignInScreen();
+        },
+      ),
+    );
+  }
+}
